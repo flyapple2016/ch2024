b0384003671ebe75c6152d9ce9a501f72bf0894d
Kamren <23100346+flyapple2016@users.noreply.github.com>
2026-01-19 15:22:32 +0800

    Update W11-ISO.ps1

--- W11-ISO.ps1
+++ W11-ISO.ps1
@@ -33,8 +33,9 @@ $SevenZip = "7z.exe"
 
 $DecryptCmd = "decrypt.cmd"
 $Content = Get-Content $DecryptCmd
-$NewContent = $Content -replace '^set AutoStart=0$', 'set AutoStart=1'
-$NewContent = $Content -replace '^MultiChoice=1$', 'MultiChoice=0'
+$NewContent = $Content `
+    -replace '^set AutoStart=0$', 'set AutoStart=1' `
+    -replace '^set MultiChoice=1$', 'set MultiChoice=0'
 $NewContent | Set-Content $DecryptCmd -Encoding ASCII
 
 cmd /c "`"$pwd\$DecryptCmd`" `"$pwd\$ESDFile`""
